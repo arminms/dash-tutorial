52bf772f9d18a12b4dd6ca4164919998a9cb99a3
Armin Sobhani <asobhani@sharcnet.ca>
2020-07-27 17:24:40 -0400

    add color coded output for each process for examples

--- example/generate_random.cpp
+++ example/generate_random.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <algorithm>
 #include <random>
+#include <string>
+#include <cmath>
 
 #include <libdash.h>
 
@@ -8,19 +10,33 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+    int N = 512;
+
     dash::init(&argc, &argv);
 
-    dash::Array<int> arr(512);
+    if (argc > 1)
+        N = stoi(argv[1]);
+
+    dash::Array<int> arr(N);
 
     std::mt19937 rd(static_cast<uint>(dash::myid()));
-    std::uniform_real_distribution<float> dist(0, 100);
+    // std::uniform_real_distribution<float> dist(0, 1);
+    std::uniform_int_distribution<int> dist(0, 100);
 
     std::generate(arr.lbegin(), arr.lend(), [&] () { return dist(rd); });
+    // dash::generate(arr.begin(), arr.end(), [&] () { return dist(rd); });
 
     if (dash::myid() == 1)
     {
-        for (auto val : arr)
-            cout << static_cast<int>(val) << " ";
+        int color = 9;
+        cout << "N = " << N << ", Number of Processes = " << dash::size();
+        for (size_t i = 0; i < arr.size(); ++i)
+        {
+            if (0 == i % size_t(ceil(float(N) / float(dash::size()))))
+                cout << "\033[38;5;" << color++ << "m" << endl;
+            cout << setw(4)
+                 << static_cast<int>(arr[i]);
+        }
         cout << endl;
     }
 
